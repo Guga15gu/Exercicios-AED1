761ac3a801770ec3e6e59c16bd5c52f3ccbbf971
Gustavo da Silva Machado <48689692+Guga15gu@users.noreply.github.com>
2019-04-03 10:45:04 -0300

    Add files via upload

---
+++ 6.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct pessoa{
+	
+	char nome[20];
+	char data[20];
+	int cpf;
+		
+};
+
+void preencher(struct pessoa *a){
+	
+	printf("Digite o nome: ");
+	scanf("%s", (*a).nome);
+	
+	printf("\nDigite a data de nascimento: ");
+	scanf("%s", (*a).data);
+	
+	printf("\nDigite o cpf da pessoa: ");
+	scanf("%d", &(*a).cpf);
+	
+}
+
+void imprimir(struct pessoa *a){
+	
+
+	
+	printf("\nNome: %s ", (*a).nome);
+	
+	printf("\nData de nascimento: %s", (*a).data);
+
+	printf("\nCpf da pessoa: %d", (*a).cpf);
+	
+}
+
+int main(int argc, char **argv)
+{	
+	struct pessoa a;
+	
+	preencher( &a );
+	imprimir( &a );
+	
+	return 0;
+}
+

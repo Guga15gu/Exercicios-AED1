d45d60b8ca84df78dd2ecb9a58bba760757d9a77
Gustavo da Silva Machado <48689692+Guga15gu@users.noreply.github.com>
2019-05-06 15:57:55 -0300

    Update BeO-6.c

--- BeO-6.c
+++ BeO-6.c
@@ -6,44 +6,46 @@ void AddOrdenado( int *n, int *vetor, int elemento ){
 	int a=0;
 	vetor = realloc(vetor, sizeof(int) * *n);
 	while ( a<*n ){
-		
+
 		if(elemento	<= vetor[a]){
 
-			int buffer = vetor[a];
-			for(; a< *n; ++a){
-				
-				vetor[a] = elemento;
-				elemento= vetor[a+1];
-				vetor[a+1] = buffer;
-			}		
-		}	
-		++a;	
-	}		
+			for(int b = *n; b>a; --b){
+
+                vetor[b] = vetor[b-1];
+
+			}
+        vetor[a] = elemento;
+        a = *n;
+		}
+		a++;
+	}
 }
 
 int main(int argc, char **argv)
 {
 	int n;
-	int elemento;
-	
+
 	printf("Digite o tamanho do vetor: ");
 	scanf("%d", &n);
-	
+
 	int *vetor;
 	vetor = malloc(sizeof(int) * n);
-	
-	for(int a=0; a< n; ++a){	
+
+	for(int a=0; a< n; ++a){
 		vetor[a] =  a;
 		printf("\n vetor[%d]: %d", a, vetor[a]);
 	}
-	elemento = 5;
-	
-	AddOrdenado( &n, vetor, elemento );
-	
-	for(int a=0; a< n; ++a){
-		
-		printf("\n vetor[%d]: %d", a, vetor[a]);
-	}
+
+    for(int b=0; b<10; ++b){
+
+        AddOrdenado( &n, vetor, b );
+        for(int a=0; a< n; ++a){
+
+            printf("\n vetor[%d]: %d", a, vetor[a]);
+        }
+    }
+
+
+
 	return 0;
 }
-

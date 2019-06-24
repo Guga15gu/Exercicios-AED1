32b65305e8925c147f83b3bc26abdc46cbfa158e
Gustavo da Silva Machado <48689692+Guga15gu@users.noreply.github.com>
2019-06-23 22:48:50 -0300

    Add files via upload

---
+++ arvore.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+
+typedef struct registro{
+    char nome[30];
+    int idade;
+}registro;
+
+typedef No *Apontador;
+
+typedef struct No{
+     registro dado;
+     Apontador pEsq, pDir;
+
+  }No;
+
+void insere(registro x, Apontador *p){
+
+    if(*p == NULL){
+        *p = (Apontador) malloc (sizeof(No));
+        (*p)-> registro = x;
+        (*p)->pEsq = NULL;
+        (*p)->pDir = NULL;
+        return;
+    }
+    if(x.idade < (*p)->registro.idade){
+        insere(x, &(*P)->pEsq);
+        return;
+    }
+    if(x.idade > (*p)->registro.idade){
+        insere(x, &(*P)->pDir);
+        return;
+    }
+    else{
+        printf("Erro: Registro já existente.\n");
+    }
+}
+
+void  inicializa(Apontador *agenda){
+    *agenda = NULL;
+}
+
+void retira(registro x, Apontador *p){
+    Apontador aux;
+
+    if(*p == NULL){
+        printf("Erro: Registro não existente.\n");
+        return;
+    }
+    if(x.idade < (*p)->registro.idade){
+        Retira(x, &(*P)->pEsq);
+        return;
+    }
+    if(x.idade > (*p)->registro.idade){
+        Retira(x, &(*P)->pDir);
+        return;
+    }
+    if((*p)->pDir == NULL){
+        aux = *p;
+        *p = (*p)->pEsq;
+        free(aux);
+        return;
+    }
+
+    if((*p)->pEsq != NULL){
+
+    }
+
+    aux = *p;
+    *p  = (*p)-> pDir;
+    free(aux);
+    
+}
+
+void antecessor(Apontador q, Apontador *r){
+
+    Apontador aux;
+
+    if((*r)->pDir != NULL){
+        Antecessor(q, &(*r)->pDir);
+        return;
+    }
+    q->
+}
+int main(){ 
+
+
+
+    return 0;
+}
